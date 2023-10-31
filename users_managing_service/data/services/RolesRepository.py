07eb61ed0e0cd13c73c224c960e8f935baf13758
Denis Voitenko <denis@denvot.dev>
2023-11-01 01:29:29 +0300

    RolesRepository

---
+++ users_managing_service/data/services/RolesRepository.py
@@ -0,0 +1,16 @@
+from abc import ABC
+
+
+class RolesRepository(ABC):
+    def create_role(self, role_name: str) -> RoleData:
+        raise NotImplementedError()
+    
+    def delete_role(self, role_id: int):
+        raise NotImplementedError()
+    
+    def get_role(self, role_id: int):
+        raise NotImplementedError()
+    
+    def get_all_roles(self) -> RoleData:
+        raise NotImplementedError()
+    
