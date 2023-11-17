ec8d53d60ee1d66c65b6866333f3d02fd2f0a432
Nicolas <nikoboga90@gmail.com>
2023-11-17 21:23:44 +0300

    registration setup

--- telegram_interactions_service/handlers/user/registration.py
+++ telegram_interactions_service/handlers/user/registration.py
@@ -1,14 +1,68 @@
 from aiogram import Router, F, Dispatcher
 from aiogram.filters import Command
 from aiogram.types import Message
+from aiogram.fsm.context import FSMContext
+from telegram_interactions_service.states.registration_states import RegistrationForm
+from telegram_interactions_service.middlewares.registration_middleware import IsUnregisteredMiddleware
+from telegram_interactions_service.misc.dataclasses import InputUserData
+from telegram_interactions_service.exceptions import BadRegistrationInput
+from telegram_interactions_service.services_interactions.user_managing_service import UserManagingServiceInteraction
+from typing import NoReturn
 
 registration_router = Router()
 
 
 @registration_router.message(Command("reg"))
-async def cmd_hello(message: Message):
-    await message.answer("Привет мир!")
+async def cmd_reg(message: Message, state: FSMContext) -> NoReturn:
+    await message.answer("Привет! Введите ваше имя:")
+    await state.set_state(RegistrationForm.name)
+
+
+@registration_router.message(Command("cancel"))
+@registration_router.message(F.text.casefold() == "cancel")
+async def cancel_handler(message: Message, state: FSMContext) -> NoReturn:
+    current_state = await state.get_state()
+    if current_state is None:
+        return
+    await state.clear()
+    await message.answer("Регистрация прервана!")
+
+
+@registration_router.message(RegistrationForm.name)
+async def receive_name(message: Message, state: FSMContext) -> NoReturn:
+    await state.update_data(name=message.text)
+    await state.set_state(RegistrationForm.surname)
+    await message.answer("Введите вашу фамилию:")
+
+
+@registration_router.message(RegistrationForm.surname)
+async def receive_surname(message: Message, state: FSMContext) -> NoReturn:
+    await state.update_data(surname=message.text)
+    await state.set_state(RegistrationForm.group)
+    await message.answer("Введите свою группу:")
+
+
+@registration_router.message(RegistrationForm.group)
+async def receive_group(message: Message, state: FSMContext) -> NoReturn:
+    await state.update_data(group=message.text)
+    await state.set_state(RegistrationForm.email_address)
+    await message.answer("Введите свою электронную почту физтеха:")
+
+
+@registration_router.message(RegistrationForm.email_address)
+async def receive_email_address(message: Message, state: FSMContext) -> NoReturn:
+    user_data = await state.get_data()
+    await state.clear()
+    try:
+        user = InputUserData(**user_data, email_address=message.text, tg_id=message.from_user.id)
+    except BadRegistrationInput as error:
+        await message.answer("Данные введены в неверном формате! Пройдите регистрацию заново!")
+        return
+    user_managing_service = UserManagingServiceInteraction()
+    user_managing_service.add_user_to_database(user)
+    await message.answer(f"{user_data['name'].capitalize()}, Вы успешно зарегистрированы!")
 
 
 def setup(*, dispatcher: Dispatcher):
+    registration_router.message.middleware(IsUnregisteredMiddleware())
     dispatcher.include_router(registration_router)
--- telegram_interactions_service/misc/dataclasses.py
+++ telegram_interactions_service/misc/dataclasses.py
@@ -1,23 +1,22 @@
-from dataclasses import dataclass
+from pydantic import BaseModel, validator
 from datetime import datetime
 from typing import List
+import re
+from telegram_interactions_service.exceptions import BadRegistrationInput
 
 
-@dataclass
-class Role:
+class Role(BaseModel):
     id: int
     name: str
     is_admin: bool
 
 
-@dataclass
-class StudentInfo:
+class StudentInfo(BaseModel):
     room_number: int
     is_male: bool
 
 
-@dataclass
-class User:
+class User(BaseModel):
     name: str
     surname: str
     tg_id: int
@@ -26,20 +25,17 @@ class User:
     student_info: StudentInfo
 
 
-@dataclass
-class Form:
+class Form(BaseModel):
     id: int
     name: str
 
 
-@dataclass
-class StudentInfoDelta:
+class StudentInfoDelta(BaseModel):
     room_number: int
     is_male: bool
 
 
-@dataclass
-class UserDelta:
+class UserDelta(BaseModel):
     new_name: str
     new_surname: str
     new_role_id: int
@@ -47,26 +43,58 @@ class UserDelta:
     new_score: int
 
 
-@dataclass
-class FormTicket:
+class FormTicket(BaseModel):
     form: Form
     author: User
     creation_date: datetime
 
 
-@dataclass
-class Bill:
+class Bill(BaseModel):
     photo: str
 
 
-@dataclass
-class MaterialHelpTicket:
+class MaterialHelpTicket(BaseModel):
     author: User
     bills: List[Bill]
     creation_data: datetime
 
 
-@dataclass
-class NotifyCategory:
+class NotifyCategory(BaseModel):
     id: int
     name: str
+
+
+class InputUserData(BaseModel):
+    name: str
+    surname: str
+    group: str
+    email_address: str
+    tg_id: int
+
+    @validator("name")
+    @classmethod
+    def validate_name(cls, value: str) -> str:
+        if not value.isalpha():
+            raise BadRegistrationInput("Name must contain only letters")
+        return value
+
+    @validator("surname")
+    @classmethod
+    def validate_surname(cls, value: str) -> str:
+        if not value.isalpha():
+            raise BadRegistrationInput("Surname must contain only letters")
+        return value
+
+    @validator("group")
+    @classmethod
+    def validate_group(cls, value: str) -> str:
+        if not bool(re.fullmatch(r'\w\d\d-\d\d\d', value)):
+            raise BadRegistrationInput("Group must be special format")
+        return value
+
+    @validator("email_address")
+    @classmethod
+    def validate_email(cls, value: str) -> str:
+        if not bool(re.fullmatch(r"^[-\w\.]+@phystech.edu", value)):
+            raise BadRegistrationInput("Email is invalid")
+        return value
