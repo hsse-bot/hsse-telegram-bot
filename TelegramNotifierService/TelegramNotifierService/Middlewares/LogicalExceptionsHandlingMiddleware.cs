5d4da42fcb0ae66fe1abc8e24d6abffb8bff637a
Denis Voitenko <denis@denvot.dev>
2023-11-18 00:17:33 +0300

    Добавление ПО промежуточного слоя для обработки исключений

---
+++ TelegramNotifierService/TelegramNotifierService/Middlewares/LogicalExceptionsHandlingMiddleware.cs
@@ -0,0 +1,32 @@
+﻿using TelegramNotifierService.Data.Api.Responses;
+using TelegramNotifierService.Exceptions;
+
+namespace TelegramNotifierService.Middlewares;
+
+public class LogicalExceptionsHandlingMiddleware
+{
+    private const int BadRequestStatusCode = 401;
+    
+    private readonly RequestDelegate _next;
+    private readonly ILogger<LogicalExceptionsHandlingMiddleware> _logger;
+
+    public LogicalExceptionsHandlingMiddleware(RequestDelegate next,
+        ILogger<LogicalExceptionsHandlingMiddleware> logger)
+    {
+        _next = next;
+        _logger = logger;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch (LogicalException e)
+        {
+            context.Response.StatusCode = BadRequestStatusCode;
+            await context.Response.WriteAsJsonAsync(ErrorResponse.FromException(e));
+        }
+    }
+}
