9b7f5d68e008d3f913c60d7ea2a26173aec7a028
marybal7 <panda.lisa.kotik@yandex.ru>
2023-11-24 09:07:07 +0300

    Создание сущностей БД

---
+++ users_managing_service/data/db/entities/Role.py
@@ -0,0 +1,10 @@
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
+from users_managing_service.data.db import Base
+
+
+class Role(Base):
+    __tablename__ = "roles"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    role_name: Mapped[str] = mapped_column(String(32))
---
+++ users_managing_service/data/db/entities/User.py
@@ -0,0 +1,20 @@
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
+from sqlalchemy import String
+from typing import Optional
+from users_managing_service.data.db import Base
+from users_managing_service.data.db.entities import StudentInfo
+from users_managing_service.data.db.entities import Role
+
+
+class User(Base):
+    __tablename__ = "user_account"
+
+    name: Mapped[str] = mapped_column(String(32))
+    surname: Mapped[str] = mapped_column(String(32))
+    role_id: Mapped[int] = mapped_column(primary_key=True)
+    student_info_id: Mapped[Optional[int]]
+    tg_id: Mapped[int] = mapped_column()
+    role: Mapped[Role] = mapped_column()
+    student_info: Mapped[StudentInfo] = mapped_column()
+    score: Mapped[int] = mapped_column()
