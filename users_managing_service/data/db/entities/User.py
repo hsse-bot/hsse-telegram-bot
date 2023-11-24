7bde050ef2c0506f68b7339c22b20ebb856a5388
marybal7 <panda.lisa.kotik@yandex.ru>
2023-11-24 09:31:09 +0300

    Добавление иниициализации по умолчанию

--- users_managing_service/data/db/entities/User.py
+++ users_managing_service/data/db/entities/User.py
@@ -13,7 +13,7 @@ class User(Base):
     name: Mapped[str] = mapped_column(String(32))
     surname: Mapped[str] = mapped_column(String(32))
     role_id: Mapped[int] = mapped_column(primary_key=True)
-    student_info_id: Mapped[Optional[int]]
+    student_info_id: Mapped[Optional[int]] = mapped_column()
     tg_id: Mapped[int] = mapped_column()
     role: Mapped[Role] = mapped_column()
     student_info: Mapped[StudentInfo] = mapped_column()
