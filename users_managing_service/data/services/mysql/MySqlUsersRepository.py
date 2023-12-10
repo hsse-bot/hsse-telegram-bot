2d8a529744a9ad0ffa695536f074677f4538acc1
marybal7 <144066336+marybal7@users.noreply.github.com>
2023-12-10 17:11:32 +0300

    исправление ошибок

--- users_managing_service/data/services/mysql/MySqlUsersRepository.py
+++ users_managing_service/data/services/mysql/MySqlUsersRepository.py
@@ -5,8 +5,11 @@ from sqlalchemy import update
 from sqlalchemy.orm import Session
 from data.db.Base import Base
 from data.common.UserData import UserData
+from data.common.StudentInfoData import StudentInfoData
 from data.common.UserDelta import UserDelta
 from data.db.entities.User import User
+from data.db.entities.StudentInfo import StudentInfo
+from data.common.StudentInfoData import StudentInfoData
 
 DATABASE_NAME = 'application.mysql'
 
@@ -21,42 +24,74 @@ class MySqlUsersRepository(Base):
                 name=user_data.name,
                 surname=user_data.surname,
                 role_id=user_data.role.id,
-                student_info_id=user_data.student_info.id,
                 tg_id=user_data.tg_id,
                 role=user_data.role,
-                student_info=user_data.student_info,
                 score=user_data.score
             )
+            self.check_student_info_data(user, user_data)
             session.add(user)
             session.commit()
 
     def get_user(self, tg_id: int) -> UserData:
         with Session(engine) as session:
             found_user = session.query(User).filter(User.tg_id == tg_id)[0]
-            session.commit()
         return found_user
 
     def get_all_users(self) -> List[UserData]:
         all_users = []
         with Session(engine) as session:
-            for row in session.execute(select(UserData)).first():
-                all_users.append(row)
-            session.commit()
+            for user in session.scalars(select(User)):
+                user_data = UserData()
+                user_data.name = user.name
+                user_data.surname = user.surname
+                user_data.tg_id = user.tg_id
+                user_data.role.id = user.role.id
+                user_data.role.name = user.role.role_name
+                self.check_student_info(user, user_data)
+                user_data.score = user.score
+                all_users.append(user_data)
         return all_users
 
     def update_user(self, tg_id: int, delta: UserDelta) -> NoReturn:
         with Session(engine) as session:
-            update(UserData).where(User.tg_id == tg_id).values(
-                name=delta.new_name,
-                surname=delta.new_surname,
-                role_id=delta.new_role_id,
-                student_info=delta.student_info_delta,
-                score=delta.new_score
+            user = session.query(User).filter(User.tg_id == tg_id)[0]
+            update(User).where(User.tg_id == tg_id).values(
+                name=delta.new_name if delta.new_name else user.name,
+                surname=delta.new_surname if delta.new_surname else user.surname,
+                role_id=delta.new_role_id if delta.new_role_id else user.role_id,
+                score=delta.new_score if delta.new_score else user.score
             )
-            session.commit()
+            self.check_student_info_delta(user, delta)
 
     def delete_user(self, tg_id: int) -> NoReturn:
         with Session(engine) as session:
-            user = select(UserData).where(User.tg_id == tg_id)
+            user = select(User).where(User.tg_id == tg_id)
             session.delete(user)
             session.commit()
+
+    def check_student_info_delta(self, user: User, user_delta: UserDelta) -> NoReturn:
+        if (user_delta.student_info_delta != None):
+            if (user_delta.student_info_delta.new_room_number != None):
+                new_room_number = user_delta.student_info_delta.new_room_number
+                user.student_info.room_number = new_room_number
+            if (user_delta.student_info_delta.new_is_male != None):
+                new_is_male = user_delta.student_info_delta.new_is_male
+                user.student_info.is_male = new_is_male
+
+    def check_student_info_data(self, user: User, user_data: UserData) -> NoReturn:
+        if (user_data.student_info != None):
+            if (user_data.student_info.room_number != None):
+                room_number = user_data.student_info.room_number
+                user.student_info.room_number = room_number
+            if (user_data.student_info.is_male != None):
+                is_male = user_data.student_info.is_male
+                user.student_info.is_male = is_male
+
+    def check_student_info(self, user: User, user_data: UserData) -> NoReturn:
+        if (user.student_info != None):
+            if (user.student_info.room_number != None):
+                room_number = user.student_info.room_number
+                user_data.student_info.room_number = room_number
+            if (user.student_info.is_male != None):
+                is_male = user.student_info.is_male
+                user_data.student_info.is_male = is_male
