ca70b05c883965ce716c52010ac895569b410783
marybal7 <144066336+marybal7@users.noreply.github.com>
2023-12-10 18:50:32 +0300

    Update User.py

--- users_managing_service/data/db/entities/User.py
+++ users_managing_service/data/db/entities/User.py
@@ -12,9 +12,9 @@ class User(Base):
 
     name: Mapped[str] = mapped_column(String(32))
     surname: Mapped[str] = mapped_column(String(32))
-    role_id: Mapped[int] = mapped_column(primary_key=True)
+    role_id: Mapped[int] = mapped_column()
     student_info_id: Mapped[Optional[int]] = mapped_column()
-    tg_id: Mapped[int] = mapped_column()
+    tg_id: Mapped[int] = mapped_column(primary_key=True)
     role: Mapped[Role] = mapped_column()
     student_info: Mapped[StudentInfo] = mapped_column()
     score: Mapped[int] = mapped_column()
