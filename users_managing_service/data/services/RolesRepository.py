1604a61bb52c34244a6c8d34b8cfa48d732b1f2b
Denis Voitenko <denis@denvot.dev>
2023-12-13 00:07:58 +0300

    Рефакторинг

--- users_managing_service/data/services/RolesRepository.py
+++ users_managing_service/data/services/RolesRepository.py
@@ -1,16 +1,18 @@
 from abc import ABC
 
+from data.common.RoleData import RoleData
+
 
 class RolesRepository(ABC):
     def create_role(self, role_name: str) -> RoleData:
-        raise NotImplementedError()
+        pass
     
     def delete_role(self, role_id: int):
-        raise NotImplementedError()
+        pass
     
     def get_role(self, role_id: int):
-        raise NotImplementedError()
+        pass
     
     def get_all_roles(self) -> list[RoleData]:
-        raise NotImplementedError()
+        pass
     
--- users_managing_service/data/services/UserRepository.py
+++ users_managing_service/data/services/UserRepository.py
@@ -1,19 +1,21 @@
-import ctypes
 from abc import ABC
+
+from data.common.UserData import UserData
+from data.common.UserDelta import UserDelta
 
 
 class UserRepository(ABC):
     def create_user(self, user: UserData):
-        raise NotImplementedError()
+        pass
     
     def get_user(self, tg_id: int):
-        raise NotImplementedError()
+        pass
     
     def get_all_users(self) -> list[UserData]:
-        raise NotImplementedError()
+        pass
     
     def update_user(self, tg_id: int, delta: UserDelta) -> UserData:
-        raise NotImplementedError()
+        pass
     
     def delete_user(self, tg_id: int):
-        raise NotImplementedError()
+        pass
