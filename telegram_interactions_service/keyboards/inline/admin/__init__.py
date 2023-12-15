e00df12bd30b39e5c7f2de7d65f1b62439f75d64
Alexander Skripkin <skripkin.ai@phystech.edu>
2023-12-15 16:29:31 +0300

    cancel button and improved messages' texts

--- telegram_interactions_service/keyboards/inline/admin/__init__.py
+++ telegram_interactions_service/keyboards/inline/admin/__init__.py
@@ -3,11 +3,12 @@ from .material_help import admin_material_help_menu_kb
 from .notify_service import (
     admin_notify_service_menu_kb, admin_notify_category_kb, NotifyCategoriesKb, NotifyServiceMenuKb,
     notify_categories_paginator, NotifyCategoryKb, admin_return_notify_categories_kb,
-    admin_return_notify_service_menu_kb
+    admin_return_notify_service_menu_kb, admin_cancel_sending_message_category, admin_cancel_creating_category
 )
 
 __all__ = [
     "admin_main_kb", "admin_material_help_menu_kb", "admin_notify_service_menu_kb", "admin_notify_category_kb",
     "AdminMainMenuKb", "NotifyCategoriesKb", "NotifyServiceMenuKb", "notify_categories_paginator",
-    "NotifyCategoryKb", "admin_return_notify_categories_kb", "admin_return_notify_service_menu_kb"
+    "NotifyCategoryKb", "admin_return_notify_categories_kb", "admin_return_notify_service_menu_kb",
+    "admin_cancel_sending_message_category", "admin_cancel_creating_category"
 ]
