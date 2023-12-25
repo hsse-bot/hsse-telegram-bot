39e7e1ae6ab24471e6c681ff62066d76eeeeea49
Alexander Skripkin <skripkin.ai@phystech.edu>
2023-12-25 11:15:52 +0300

    try except

--- telegram_interactions_service/handlers/user/rating.py
+++ telegram_interactions_service/handlers/user/rating.py
@@ -20,7 +20,13 @@ logger = logging.getLogger(__name__)
 
 @rating_router.callback_query(user.UserMainMenuKb.filter(F.action == "/rating"))
 async def rating_handler(callback: CallbackQuery, callback_data: user.RatingMenuKb):
-    top_users = await UserManagingServiceInteraction().get_top_scores()
+    try:
+        top_users = await UserManagingServiceInteraction().get_top_scores()
+    except Exception as error:
+        logger.log(level=logging.ERROR, msg=error, exc_info=True)
+        await callback.message.edit_text(message_templates.error_user_text, reply_markup=user.error_kb())
+        await callback.answer()
+        return
     text = ''
     for number, user_in_top in enumerate(top_users):
         text += f'{number + 1}. {user_in_top.surname} {user_in_top.name} - {user_in_top.score}\n'
